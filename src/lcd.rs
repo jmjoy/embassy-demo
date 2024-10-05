4a0c92d173044bc41b8cd8fb5b96343789d79194
jmjoy <jmjoy@apache.org>
2024-10-05 23:00:15 +0800

    Some code

---
+++ src/lcd.rs
@@ -0,0 +1,191 @@
+use defmt::debug;
+use embassy_stm32::{gpio::Output, mode::Async, spi::Spi};
+use embassy_time::Timer;
+
+pub struct LCD {
+    /// SPI1外设
+    spi: Spi<'static, Async>,
+    /// 命令/数据选择
+    dc: Output<'static>,
+    /// 重置
+    res: Output<'static>,
+    /// SPI1的NSS
+    cs: Output<'static>,
+    /// 背光
+    blk: Output<'static>,
+}
+
+impl LCD {
+    pub async fn new(
+        spi: Spi<'static, Async>, dc: Output<'static>, res: Output<'static>, cs: Output<'static>,
+        blk: Output<'static>,
+    ) -> Self {
+        let mut lcd = Self {
+            spi,
+            dc,
+            res,
+            cs,
+            blk,
+        };
+        lcd.init().await;
+        lcd
+    }
+
+    async fn init(&mut self) {
+        debug!("lcd initing");
+
+        self.res.set_low(); // 复位
+        Timer::after_millis(100).await;
+        self.res.set_high();
+        Timer::after_millis(100).await;
+
+        self.blk.set_high(); // 打开背光
+        Timer::after_millis(100).await;
+
+        self.write_reg(0x11).await; // Sleep exit
+        Timer::after_millis(120).await; // Delay 120ms
+
+        // Frame Rate Control (In normal mode/ Full colors)
+        self.write_reg(0xB1).await;
+        self.write_data(0x05).await;
+        self.write_data(0x3C).await;
+        self.write_data(0x3C).await;
+
+        // Frame Rate Control (In Idle mode/ 8-colors)
+        self.write_reg(0xB2).await;
+        self.write_data(0x05).await;
+        self.write_data(0x3C).await;
+        self.write_data(0x3C).await;
+
+        // Frame Rate Control (In Partial mode/ full colors)
+        self.write_reg(0xB3).await;
+        self.write_data(0x05).await;
+        self.write_data(0x3C).await;
+        self.write_data(0x3C).await;
+        self.write_data(0x05).await;
+        self.write_data(0x3C).await;
+        self.write_data(0x3C).await;
+
+        // Display Inversion Control
+        self.write_reg(0xB4).await; // Dot inversion
+        self.write_data(0x03).await;
+
+        // Power Control 1
+        self.write_reg(0xC0).await;
+        self.write_data(0x0E).await;
+        self.write_data(0x0E).await;
+        self.write_data(0x04).await;
+
+        // Power Control 2
+        self.write_reg(0xC1).await;
+        self.write_data(0xC5).await;
+
+        // Power Control 3 (In Normal mode)
+        self.write_reg(0xC2).await;
+        self.write_data(0x0d).await;
+        self.write_data(0x00).await;
+
+        // Power Control 4 (In Idle mode)
+        self.write_reg(0xC3).await;
+        self.write_data(0x8D).await;
+        self.write_data(0x2A).await;
+
+        // Power Control 5 (In Partial mode)
+        self.write_reg(0xC4).await;
+        self.write_data(0x8D).await;
+        self.write_data(0xEE).await;
+
+        // VCOM Control 1
+        self.write_reg(0xC5).await; // VCOM
+        self.write_data(0x06).await; // 1D  .06
+
+        // Memory Data Access Control
+        self.write_reg(0x36).await; // MX, MY, RGB mode
+        self.write_data(0x78).await;
+
+        // Interface Pixel Format
+        self.write_reg(0x3A).await;
+        self.write_data(0x55).await;
+
+        // Gamma (‘+’polarity) Correction Characteristics Setting
+        self.write_reg(0xE0).await;
+        self.write_data(0x0b).await;
+        self.write_data(0x17).await;
+        self.write_data(0x0a).await;
+        self.write_data(0x0d).await;
+        self.write_data(0x1a).await;
+        self.write_data(0x19).await;
+        self.write_data(0x16).await;
+        self.write_data(0x1d).await;
+        self.write_data(0x21).await;
+        self.write_data(0x26).await;
+        self.write_data(0x37).await;
+        self.write_data(0x3c).await;
+        self.write_data(0x00).await;
+        self.write_data(0x09).await;
+        self.write_data(0x05).await;
+        self.write_data(0x10).await;
+
+        // Gamma ‘-’polarity Correction Characteristics Setting
+        self.write_reg(0xE1).await;
+        self.write_data(0x0c).await;
+        self.write_data(0x19).await;
+        self.write_data(0x09).await;
+        self.write_data(0x0d).await;
+        self.write_data(0x1b).await;
+        self.write_data(0x19).await;
+        self.write_data(0x15).await;
+        self.write_data(0x1d).await;
+        self.write_data(0x21).await;
+        self.write_data(0x26).await;
+        self.write_data(0x39).await;
+        self.write_data(0x3E).await;
+        self.write_data(0x00).await;
+        self.write_data(0x09).await;
+        self.write_data(0x05).await;
+        self.write_data(0x10).await;
+
+        Timer::after_millis(120).await;
+        self.write_reg(0x29).await; // Display on
+
+        debug!("lcd has init");
+    }
+
+    async fn write_reg(&mut self, reg: u8) {
+        self.dc.set_low();
+        self.write_data(reg).await;
+        self.dc.set_high();
+    }
+
+    async fn write_data(&mut self, data: u8) {
+        self.cs.set_low();
+        self.spi.write(&[data]).await.unwrap();
+        // Timer::after_micros(1).await;
+        self.cs.set_high();
+    }
+
+    async fn write_data_u16(&mut self, data: u16) {
+        self.write_data((data >> 8) as u8).await;
+        self.write_data(data as u8).await;
+    }
+
+    pub async fn fill(&mut self, x_start: u16, y_start: u16, x_end: u16, y_end: u16, color: u16) {
+        self.set_address(x_start, y_start, x_end - 1, y_end - 1)
+            .await;
+        for _ in y_start..y_end {
+            for _ in x_start..x_end {
+                self.write_data_u16(color).await;
+            }
+        }
+    }
+
+    async fn set_address(&mut self, x_start: u16, y_start: u16, x_end: u16, y_end: u16) {
+        self.write_reg(0x2a).await; // 列地址设置
+        self.write_data_u16(x_start).await;
+        self.write_data_u16(x_end).await;
+        self.write_reg(0x2b).await; // 行地址设置
+        self.write_data_u16(y_start + 24).await;
+        self.write_data_u16(y_end + 24).await;
+        self.write_reg(0x2c).await; // 储存器写
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 #![no_std]
 #![no_main]
 
+mod lcd;
+mod w25q64;
+
 use core::sync::atomic::{AtomicIsize, Ordering};
 use defmt::info;
 use defmt_rtt as _;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
+use embassy_stm32::{
+    gpio::{Input, Level, Output, Pull, Speed},
+    mode::Async,
+    pac,
+    spi::{self, Spi},
+    time,
+};
 use embassy_time::Timer;
+use lcd::LCD;
 use panic_probe as _;
+use w25q64::W25Q64;
 
 static NUM: AtomicIsize = AtomicIsize::new(0);
 
@@ -16,20 +27,45 @@ async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
     info!("点灯大师，启动!");
 
+    // BTN
     let led = Output::new(p.PC13, Level::High, Speed::VeryHigh);
     let btn = Input::new(p.PA1, Pull::Up);
-
     spawner.spawn(handle_num(btn, led)).unwrap();
 
+    // W25Q64
+    let nss = Output::new(p.PB12, Level::High, Speed::VeryHigh);
+    let mut spi_config = spi::Config::default();
+    spi_config.mode = spi::MODE_0;
+    spi_config.bit_order = spi::BitOrder::MsbFirst;
+    let spi = Spi::new(
+        p.SPI2, p.PB13, p.PB15, p.PB14, p.DMA1_CH5, p.DMA1_CH4, spi_config,
+    );
+    spawner.spawn(w25q46_task(spi, nss)).unwrap();
+
+    // LCD
+    pac::AFIO.mapr().modify(|w| {
+        w.set_swj_cfg(0b0000_0010); // this is equal to __HAL_AFIO_REMAP_SWJ_NOJTAG() in C
+        w.set_spi1_remap(true);
+    });
+    let mut spi_config = spi::Config::default();
+    spi_config.mode = spi::MODE_3;
+    spi_config.bit_order = spi::BitOrder::MsbFirst;
+    spi_config.frequency = time::mhz(8 / 2);
+    let spi = Spi::new_txonly(p.SPI1, p.PB3, p.PB5, p.DMA1_CH3, spi_config);
+    let dc = Output::new(p.PB4, Level::High, Speed::VeryHigh);
+    let res = Output::new(p.PB6, Level::High, Speed::VeryHigh);
+    let cs = Output::new(p.PB7, Level::High, Speed::VeryHigh);
+    let blk = Output::new(p.PB8, Level::High, Speed::VeryHigh);
+    spawner.spawn(show_lcd(spi, dc, res, cs, blk)).unwrap();
+
+    // Print
     loop {
         Timer::after_secs(1).await;
         info!("NUM: {}", NUM.load(Ordering::Relaxed));
     }
 }
 
-/**
- * 软件消抖
- */
+/// 软件消抖
 #[embassy_executor::task]
 async fn handle_num(btn: Input<'static>, mut led: Output<'static>) {
     let mut last_level = btn.get_level();
@@ -45,4 +81,26 @@ async fn handle_num(btn: Input<'static>, mut led: Output<'static>) {
         }
         last_level = now_level;
     }
+}
+
+#[embassy_executor::task]
+async fn show_lcd(
+    spi: Spi<'static, Async>, dc: Output<'static>, res: Output<'static>, cs: Output<'static>,
+    blk: Output<'static>,
+) {
+    let mut lcd = LCD::new(spi, dc, res, cs, blk).await;
+    lcd.fill(0, 0, 160, 80, 0xFC07).await;
+    loop {
+        Timer::after_secs(6).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn w25q46_task(spi: Spi<'static, Async>, nss: Output<'static>) {
+    let mut w25q64 = W25Q64::new(spi, nss);
+    let jedec = w25q64.read_jedec().await;
+    info!(
+        "w25q64 jedec: vendor: {}, device: {}",
+        jedec.vendor_id, jedec.device_id
+    );
 }
